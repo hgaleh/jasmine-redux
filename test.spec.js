7a430702b66b5820b28473083b33049d2dbb5e2f
Bakhtiyari <h.galeh@gmail.com>
2020-07-12 17:53:16 +0430

    added jtest

---
+++ test.spec.js
@@ -0,0 +1,30 @@
+import { createStore } from 'redux'
+
+function counter(state = 0, action) {
+  switch (action.type) {
+      case 'INCREMENT':
+      return state + 1
+      case 'DECREMENT':
+      return state - 1
+      default:
+      return state
+  }
+}
+
+let store;
+
+describe('actions', () => {
+  beforeEach(() => {
+    store = createStore(counter);
+  });
+
+  it('simple test increment', () => {
+    store.dispatch({type: 'INCREMENT'});
+    expect(store.getState()).toBe(1);
+  });
+
+  it('simple test decrement', () => {
+    store.dispatch({type: 'DECREMENT'});
+    expect(store.getState()).toBe(-1);
+  });
+})

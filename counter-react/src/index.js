be49ac5f535d6e79e7f15029bf5657551448fe5c
Bakhtiyari <h.galeh@gmail.com>
2020-07-13 15:36:52 +0430

    made redux increment with react

--- counter-react/src/index.js
+++ counter-react/src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import counter from './reducers/default';
+import Counter from './components/Counter';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const store = createStore(counter);
+
+const render = () => ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter 
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+render();
+store.subscribe(render);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
